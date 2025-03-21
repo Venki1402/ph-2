add5d66da73ab28b12282cadf5980ea3653662ee
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-03-21 10:38:01 +0530

    backend WIP

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,96 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, verify } from "hono/jwt";
 
-const app = new Hono()
+// Create the main Hono app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Auth Middleware
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("Authorization") || "";
+  const token = header.split(" ")[1];
+  const response = await verify(token, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: "unauthorized" });
+  }
+});
 
-export default app
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        username: body.username,
+      },
+    });
+    const token = await sign({ id: user.id }, c.env?.JWT_SECRET);
+    return c.json({
+      token,
+    });
+  } catch (e) {
+    return c.status(403);
+  }
+});
+
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({
+        error: "user not found",
+      });
+    }
+
+    const token = await sign({ id: user.id }, c.env?.JWT_SECRET);
+    return c.json({
+      token,
+    });
+  } catch (e) {
+    return c.status(403);
+  }
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("get blog route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("post blog route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("update blog route");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Landing page");
+});
+
+export default app;
