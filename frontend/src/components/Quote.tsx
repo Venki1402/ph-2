b73aa11432364c4e5b51e6b4c329556e775e09a3
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-03-23 12:32:39 +0530

    frontend WIP

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,10 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-100 h-screen flex flex-col justify-center">
+      <div>
+        "Power isn't determined by your size, but the size of your heart and
+        dreams"
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/pages/Signin.tsx
@@ -0,0 +1,10 @@
+import { Quote } from "../components/Quote";
+
+export const Signin = () => {
+  return (
+    <div className="grid grid-cols-2">
+      <div>signin</div>
+      <Quote />
+    </div>
+  );
+};
